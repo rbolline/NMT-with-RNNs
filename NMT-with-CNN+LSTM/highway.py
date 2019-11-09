2bdff809225179d2373e69638d726b03a69be935
Rohith Bollineni <Rohith@rojass-MacBook-Air.local>
2019-11-08 21:10:11 -0800

    included assignment using CNN encoder and LSTM decoder

---
+++ NMT-with-CNN+LSTM/highway.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+### YOUR CODE HERE for part 1d
+import torch
+import torch.nn as nn
+import torch.nn.utils
+import torch.nn.functional as F
+
+class Highway(nn.Module): 
+    def __init__(self):#ADD PARAMS
+
+        super(Highway, self).__init__()
+
+        #Initialize weight matrices
+        
+
+
+    def forward(self, input):
+        
+
+### END YOUR CODE 
+
